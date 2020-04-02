ed5d2083b228f63aa739ff2d4525c8d20615cf53
octavian-ilie <60600237+octavian-ilie@users.noreply.github.com>
2020-04-02 14:16:18 +0200

    Created the app header

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import './Board.css';
 
+// Temporary location for development purposes
+const mockLocation = 'Amsterdam';
+
 export class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      spinner: 'Loading...',
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData(`/api/weather/${mockLocation}`);
+  }
+
+  fetchData = (url) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => this.setState({ data }));
+  }
+  
   render() {
+
+    if (!this.state.data) {
+      return (
+        <div>
+          <p> {this.state.spinner} </p>
+        </div>
+      )
+    }
+
     return (
       <div>
         <h2>The weather board.</h2>
       </div>
     )
+
   }
 }
 
