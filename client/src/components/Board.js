9d5dfd13df146d0e08f6f4d66421e8f8b3d9860c
octavian-ilie <60600237+octavian-ilie@users.noreply.github.com>
2020-04-02 12:08:36 +0200

    Created React weather Board.

---
+++ client/src/components/Board.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Board.css';
+
+export class Board extends Component {
+  render() {
+    return (
+      <div>
+        <h2>The weather board.</h2>
+      </div>
+    )
+  }
+}
+
+export default Board;
