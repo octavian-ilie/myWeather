aacbbcc1e5e8f25c89b19132af790d3d0e19c913
octavian-ilie <60600237+octavian-ilie@users.noreply.github.com>
2020-04-02 17:35:10 +0200

    User input has style for mobile devices

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -7,7 +7,6 @@ export class Board extends Component {
     super(props);
     this.state = {
       spinner: 'Loading...',
-      reload: false,
       location: 'Amsterdam',
       data: null,
     };
@@ -16,7 +15,7 @@ export class Board extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    this.setState({ location: this.element.value, reload: true });
+    this.setState({ location: this.element.value });
   }
 
   getWeather(location) {
@@ -54,12 +53,16 @@ export class Board extends Component {
       <div className="board">
         <Location data={this.state.data.location}/>
         <br></br><br></br><br></br><br></br>
-        <form onSubmit={this.handleSubmit.bind()}>
-        <label>
-          <input type="text" ref={ el => this.element = el } />
-        </label>
-        <input type="submit" value="GO" />
-      </form>
+        
+        <div className="user-input">
+          <form onSubmit={this.handleSubmit.bind()}>
+            <label>
+              <input type="text" placeholder="Enter your location." className="text-field" ref={ el => this.element = el } />
+            </label>
+            <input type="submit" className="submit-button" value="GO" />
+          </form>
+        </div>
+
       </div>
     )
   }
