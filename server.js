e1d5d9d936d8648d14183d197d8ba1ae3a09cf46
octavian-ilie <60600237+octavian-ilie@users.noreply.github.com>
2020-04-02 10:39:43 +0200

    Created skeleton for Express and React.

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express');
 
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('The local server is working just fine.');
+});
+
+app.get('/api/weather/:location', (req, res) => {
+  const { location } = req.params;
+  res.send(`The weather from ${location} will be shown here.`);
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}.`));
